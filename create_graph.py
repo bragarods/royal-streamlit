4c7fc3af74c99aecba2506560e9ad4161cb96bc7
Rodrigo Braga <ramosrods@gmail.com>
2023-07-22 21:33:29 +0000

    initial streamlit version

--- create_graph.py
+++ create_graph.py
@@ -2,24 +2,40 @@
 import pandas as pd
 import networkx as nx
 
-# read csv file
-df = pd.read_csv('data/royal92.csv')
+def get_graph():
+    """Create NetworkX graph from data in csv file."""
+    # read csv file
+    df = pd.read_csv('data/royal92.csv')
 
-# create graph
-G = nx.Graph()
+    # create graph
+    G = nx.Graph()
 
-# add nodes
-for i, row in df.iterrows():
-    G.add_node(
-        row['pointer'],
-        name=row['name'],
-        first=row['first'],
-        last=row['last'],
-        birth=row['birth'],
-        death=row['death'],
-        mother=row['mother'],
-        father=row['father']
-        )
+    # add nodes
+    for i, row in df.iterrows():
+        G.add_node(
+            row['pointer'],
+            name=row['name'],
+            first=row['first'],
+            last=row['last'],
+            birth=row['birth'],
+            death=row['death'],
+            mother=row['mother'],
+            father=row['father']
+            )
 
-# print # of nodes
-print(f'Number of nodes: {G.number_of_nodes()}')
+    # print # of nodes
+    print(f'# of nodes added: {G.number_of_nodes()}')
+
+    # add edges
+    for i, row in df.iterrows():
+        G.add_edge(row['pointer'], row['mother'])
+        G.add_edge(row['pointer'], row['father'])
+
+    # print # of edges
+    print(f'# of edges added: {G.number_of_edges()}')
+
+    # position nodes
+    pos = nx.spring_layout(G)
+
+    # return graph
+    return G, pos
